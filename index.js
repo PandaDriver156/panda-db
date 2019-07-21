30f763a4f7b296aae3f9ab72eac591aa48bff708
PandaDriver156 <46034537+PandaDriver156@users.noreply.github.com>
2019-07-21 23:12:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 const fs = require('fs');
 let data = {};
 
-class Database extends Map{
+class PandaDB {
 
   constructor(options = {}) {
-    super();
       options.name = String(options.name || 'PandaDB');
     options.dir = options.dir ? String(options.dir) : './database';
-  if(!fs.existsSync(options.dir)) fs.mkdirSync(options.dir);
-    Object.defineProperty(this, "size", { value: () => Object.keys(data).length});
+    if(!fs.existsSync(options.dir)) fs.mkdirSync(options.dir);
+    /*Object.defineProperty(this, "size", { value: () => Object.keys(data).length});
     Object.defineProperty(this, "options", { value: options});
     Object.defineProperty(this, "indexes", { value: () => Object.keys(data)});
-    Object.defineProperty(this, "data", { value: () => data});  
+    Object.defineProperty(this, "data", { value: () => data}); */
+    Object.defineProperties(this, {
+      size: {
+        value: () => Object.keys(data).length
+      },
+      options: {
+        value: options
+      },
+      indexes: {
+        value:  () => Object.keys(data)
+      },
+      data: {
+        value: () => data
+      },
+      version: {
+        value: require('./package.json').version
+      }
+    });
     try {
       data = require(`${process.cwd()}/${options.dir}/${options.name}.json`);
-    }catch(err) {
+    } catch {
       this.save();
     }
   }
     
     
   save() {
-    fs.writeFileSync(`${this.options.dir}/${this.options.name}.json`, JSON.stringify(data).split('},').join('},\n'));
+    fs.writeFileSync(`${process.cwd()}/${this.options.dir}/${this.options.name}.json`, JSON.stringify(data).split('},').join('},\n'));
     data = require(`${process.cwd()}/${this.options.dir}/${this.options.name}.json`);
     return data;
   }
 
   
   set(prop, value, path) {
-    if(!prop || !value) throw new Error("Prop or value missing");
+    if(!prop || !value) throw new Error("Property or value missing");
     prop = String(prop);
-    path = path ? String(path): undefined;
+    path = path ? String(path) : undefined;
     if(path) {
       path = String(path);
      if(!data[prop]) data[prop] = {};
@@ -67,8 +83,6 @@ class Database extends Map{
     else return;
   }
   
-
-  
 };
 
-module.exports = Database;
+module.exports = PandaDB;
