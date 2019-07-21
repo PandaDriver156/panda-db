eb00470bc063e386066905e91b227cd042128f47
PandaDriver156 <46034537+PandaDriver156@users.noreply.github.com>
2019-07-21 21:20:55 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+let data = {};
+
+class Database extends Map{
+
+  constructor(options = {}) {
+    super();
+      options.name = String(options.name || 'PandaDB');
+    options.dir = options.dir ? String(options.dir) : './database';
+  if(!fs.existsSync(options.dir)) fs.mkdirSync(options.dir);
+    Object.defineProperty(this, "size", { value: () => Object.keys(data).length});
+    Object.defineProperty(this, "options", { value: options});
+    Object.defineProperty(this, "indexes", { value: () => Object.keys(data)});
+    Object.defineProperty(this, "data", { value: () => data});  
+    try {
+      data = require(`${process.cwd()}/${options.dir}/${options.name}.json`);
+    }catch(err) {
+      this.save();
+    }
+  }
+    
+    
+  save() {
+    fs.writeFileSync(`${this.options.dir}/${this.options.name}.json`, JSON.stringify(data).split('},').join('},\n'));
+    data = require(`${process.cwd()}/${this.options.dir}/${this.options.name}.json`);
+    return data;
+  }
+
+  
+  set(prop, value, path) {
+    if(!prop || !value) throw new Error("Prop or value missing");
+    prop = String(prop);
+    path = path ? String(path): undefined;
+    if(path) {
+      path = String(path);
+     if(!data[prop]) data[prop] = {};
+      data[prop][path] = value;
+    } else data[prop] = value;
+    
+    this.save();
+    return data[prop];
+  }
+  
+  get(prop, path) {
+    if(!prop) throw new Error("Prop missing");
+    if(path) {
+       if (data[prop]) return data[prop][path];
+        throw new Error(`The property ${prop} does not exist in the ${this.options.name} Database`);
+    } else return data[prop];
+  }
+  
+  ensure (prop, value) {
+    if(!prop || !value) throw new Error("Prop or value missing");
+    prop = String(prop);
+    if(!data[prop]) {
+      data[prop] = value;
+      this.save();
+    }
+    return data[prop];
+  }
+  
+  delete (prop, path) {
+    if(!prop) throw new Error("Prop missing");
+    prop = String(prop);
+    path = String(path);
+    if(data[prop]) return delete data[prop];
+    else return;
+  }
+  
+
+  
+};
+
+module.exports = Database;
