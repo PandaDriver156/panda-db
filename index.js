38c9de2ceee7f85ca6c732f5f9d68c9bb7c9417f
PandaDriver156 <46034537+PandaDriver156@users.noreply.github.com>
2019-07-22 16:48:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 let data = {};
 
-class PandaDB {
+class PandaDB extends Map {
 
   constructor(options = {}) {
+    super();
     options.name = String(options.name || 'PandaDB');
     options.dir = options.dir ? String(options.dir) : './pandaDB';
     options.backup = Boolean(options.backups || true);
@@ -11,7 +12,7 @@ class PandaDB {
       options.backupInterval = options.backupInterval || 60;
 
       setInterval(function () {
-        fs.writeFileSync(`${options.dir}/backups/${options.name}.txt`, JSON.stringify(data, null, 2))
+        fs.writeFileSync(`${options.dir}/backups/${options.name}.json`, JSON.stringify(data, null, 2))
       }, options.backupInterval * 1000); 
 
       if(!fs.existsSync(`${options.dir}/backups`)) fs.mkdirSync(`${options.dir}/backups`);
@@ -21,30 +22,22 @@ class PandaDB {
     if(!fs.existsSync(options.dir)) fs.mkdirSync(options.dir);
 
     Object.defineProperties(this, {
-      size: {
-        value: () => Object.keys(data).length
-      },
       options: {
         value: options
-      },
-      indexes: {
-        value:  () => Object.keys(data)
-      },
-      data: {
-        value: () => data
       },
       version: {
         value: require('./package.json').version
       }
     });
-    
+
     try {
       data = require(`${process.cwd()}/${options.dir}/${options.name}.json`);
     } catch {
       this.save();
     }
   }
-    
+  
+
   save() {
     try {
       fs.writeFileSync(`${process.cwd()}/${this.options.dir}/${this.options.name}.json`, JSON.stringify(data, null, 2));
@@ -58,15 +51,17 @@ class PandaDB {
   set(prop, value, path) {
     if(!prop || !value) throw new Error("Property or value missing");
     prop = String(prop);
-    path = path ? String(path) : undefined;
     if(path) {
+      const propInDB = this.get(prop);
+      if(propInDB == undefined) throw new Error(`path is specified but the property "${prop}" does not exixst. Database name: ${this.name}`)
       path = String(path);
-     if(!data[prop]) data[prop] = {};
-      data[prop][path] = value;
-    } else data[prop] = value;
+      const propType = typeof propInDB;
+      if(propType != "object") throw new Error(`path is specified but the property's type is not an object. property type: ${propType}`);
+      propInDB[path] = value;
+    }
     
     this.save();
-    return this;
+    return super.set(prop, value);
   }
   
   get (prop, path) {
